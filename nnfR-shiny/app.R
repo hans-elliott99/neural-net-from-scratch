03e2de79afc65a8d13c747f5b7668f235255c0d3
Hans Elliott <hanselliott61@gmail.com>
2022-06-03 10:40:39 -0700

    finished & deployed nnfR shiny app

--- nnfR-shiny/app.R
+++ nnfR-shiny/app.R
@@ -1,23 +1,27 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
-library(hrbrthemes)
+library(ggthemes)
 library(kableExtra)
 #devtools::install_github("hans-elliott99/nnfR") ##if needed
 library(nnfR)
+##Deployment:
+#library(rsconnect)
+#rsconnect::deployApp('nnfR-shiny')
 
-# Define UI for app that draws a histogram ----
+
+# Define UI ----
 ui = fluidPage(
+  tags$style(type = "text/css", "body {padding-left: 10px;}"),
   tags$head(
     tags$style(HTML(
     "
-    @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Nuosu+SIL&family=Quicksand:wght@300&family=Roboto+Mono:wght@100&display=swap');
     body {
-    font-family: 'Quicksand', sans-serif;
+    font-family: 'Nuosu SIL', serif;
     }
     "
- 
-  ))
+  )),
   ),
   # App title ----
   titlePanel("Classification with an Artificial Neural Network", 
@@ -32,16 +36,18 @@ ui = fluidPage(
          span("Hint:", style="font-style:italic"),
          "try 2 classes for a binary classification setup)."),
       h4("Change the structure of the network by tweaking the number of neurons in
-        the hidden layer and the dropout rate"),
+        the hidden layer and the dropout rate."),
       h4("Tune the network by experimenting with different optimizer 
         hyperparameters and training epochs."),
       br(),
       p("This app was built from a custom neural network built entirely from R!
         See the nnfR package for more information."),
       br(),
-      tags$a(href="https://github.com/hans-elliott99/nnfR", "nnfR package",
+      tags$a(href="https://github.com/hans-elliott99/nnfR", target="_blank",
+             "nnfR package",
              style = "color:red"),
-      tags$a(href="https://hans-elliott99.github.io/", "by Hans Elliott")
+      tags$a(href="https://hans-elliott99.github.io/",target="_blank",
+             "by Hans Elliott")
     )
   ),
   # Sidebar layout for simulating data ----
@@ -49,6 +55,7 @@ ui = fluidPage(
     
     # Sidebar panel for inputs
     sidebarPanel(
+      h4("The Task"),
       # Input: Slider for the number of data points per class
       sliderInput(inputId = "data_points",
                   label = "Data points per class",
@@ -60,7 +67,8 @@ ui = fluidPage(
                   label = "Number of Classes",
                   min = 2,
                   max = 10,
-                  value = 3)
+                  value = 3), 
+      width = 4
     ),
     # Main panel for displaying plot outputs
     mainPanel(
@@ -85,17 +93,22 @@ ui = fluidPage(
                 label = "Dropout rate",
                 min = 0,
                 max = 0.9,
-                value = 0)
+                value = 0),
+    width = 4
     ),
     # Main panel for displaying model
     mainPanel(
+      h4("Network Structure"),
       shiny::uiOutput(outputId = "model"), width = 6
     )
   ),
   
   # Sidebar layout with inputs for optimizer ----
-  sidebarPanel(
+  sidebarLayout(
+    sidebarPanel(
     h4("Optimizer: Adam"),
+    tags$a(href = "https://machinelearningmastery.com/adam-optimization-algorithm-for-deep-learning/#:~:text=Adam%20Configuration%20Parameters,used%20with%20Adam.",
+           target="_blank", "An introduction to Adam."),
     shiny::sliderInput(inputId = "lr",
                        label = "Learning rate",
                        min = 0,
@@ -119,34 +132,46 @@ ui = fluidPage(
                         label = "Beta 2",
                         min = 0,
                         max = 0.999,
-                        value = 0.2)
-  ),
-  # mainPanel(
-  #   shiny::uiOutput(outputId = "optimizer")
-  #),
-  # Sidebar layout with training inputs ---- 
-  sidebarLayout(
-    sidebarPanel(
-      h4("Training"),
-      sliderInput(inputId = "epochs",
-                  label = "Number of Training Epochs",
-                  min = 1,
-                  max = 100,
-                  value = 10,
-                  step = 1)
+                        value = 0.2),
+    h4("Training"),
+    sliderInput(inputId = "epochs",
+                label = "Number of Training Epochs",
+                min = 1,
+                max = 100,
+                value = 10,
+                step = 1)
+    
     ),
-    # Main panel for displaying the training results
     mainPanel(
-      h3("Training Results"),
-      shiny::plotOutput(outputId = "loss_plot"),
-      shiny::plotOutput(outputId = "accuracy_plot"),
-      shiny::plotOutput(outputId = "pred_plot"),
+      shiny::plotOutput(outputId = "pred_plot"),width = 6
+    )
+  ),
+  # Sidebar layout with training inputs ---- 
+  verticalLayout(
+    # sidebarPanel(
+    #   h4("Training"),
+    #   sliderInput(inputId = "epochs",
+    #               label = "Number of Training Epochs",
+    #               min = 1,
+    #               max = 100,
+    #               value = 10,
+    #               step = 1)
+    # ),
+    # Main panel for displaying the training results
+    mainPanel(h3("Training Performance"),
+      fluidRow(
+        splitLayout(cellWidths = c("50%", "50%"), 
+                    plotOutput("loss_plot"), 
+                    plotOutput("accuracy_plot"))
+              ), width = 12
+    ),
+    fluidRow(
       h4("Confusion Matrix"),
-      h5("rows = predicted class, columns = true class"),
-      shiny::uiOutput(outputId = "conf_mat"),
-      h4("Classification Assessment"),
-      shiny::uiOutput(outputId = "performance"),
-      width = 8
+      h6("rows = predicted class, columns = true class"),
+      splitLayout(cellWidths = c("50%","50%"),
+                  shiny::uiOutput(outputId = "conf_mat"),
+                  shiny::uiOutput(outputId = "performance"),
+                  )
     )
   )
 )
@@ -199,13 +224,16 @@ server = function(input, output) {
     spiral_data %>%
       ggplot(aes(x = x, y = y, color = as.factor(label))) +
       geom_point(alpha = 0.7) +
-      hrbrthemes::scale_color_ft() +
+      ggthemes::scale_color_calc() +
+      #hrbrthemes::scale_color_ft() +
       labs(title = "Simulated Classification Task") +
-      hrbrthemes::theme_ipsum() +
+      #hrbrthemes::theme_ipsum() +
+      ggthemes::theme_tufte() + 
       theme(
         legend.position = "none",
         plot.title = element_text(size = 12),
-        panel.background = element_rect(fill = "gray95", color = "gray90")
+        panel.background = element_rect(fill = "gray98", color = "gray90"),
+        panel.grid.major = element_line(size = 0.1, color = "gray90")
       )
   })
   
@@ -262,6 +290,29 @@ server = function(input, output) {
     return(fit)
   })
   
+  #Plot predictions ----
+  output$pred_plot = renderPlot({
+    fit = fit_model()
+    spiral_data = sim_spiral()
+    spiral_data = spiral_data[,c("x","y")]
+    data = cbind(spiral_data, pred = fit$predictions)  
+    
+    data %>%
+      ggplot(aes(x = x, y = y, color = as.factor(pred))) +
+      geom_point(alpha = 0.7) +
+      ggthemes::scale_color_calc() +
+      #hrbrthemes::scale_color_ft() +
+      labs(title = "Predicted Classes") +
+      #hrbrthemes::theme_ipsum() +
+      ggthemes::theme_tufte() + 
+      theme(
+        legend.position = "none",
+        plot.title = element_text(size = 15),
+        panel.background = element_rect(fill = "gray98", color = "gray90"),
+        panel.grid.major = element_line(size = 0.1, color = "gray90")
+      )
+  })
+  
   #Plot loss ----
   output$loss_plot = renderPlot({
     fit = fit_model()
@@ -277,12 +328,18 @@ server = function(input, output) {
       ggplot(aes(x = epoch)) +
       geom_line(aes(y = loss), color = "blue") +
       geom_line(aes(y = validation_loss), color = "red") +
-      annotate("text", x = epochs-2, y = max_loss, 
+      annotate("text", x = input$epochs-2, y = max_loss, 
                label = label1, color = "blue", alpha = 0.5) +
-      annotate("text", x = epochs-2, y = sec_loss,
+      annotate("text", x = input$epochs-2, y = sec_loss,
                label = label2, color = "red", alpha = 0.5) +
-      hrbrthemes::theme_ipsum() +
-      labs(title = "Loss", y = "Loss")
+      #hrbrthemes::theme_ipsum() +
+      labs(title = "Loss", y = "Loss") +
+      ggthemes::theme_tufte() + 
+      theme(
+        legend.position = "none",
+        panel.grid.major = element_line(size = 0.1, color = "gray90")
+      )
+    
   })
   
   #Plot accuracy ----
@@ -292,40 +349,26 @@ server = function(input, output) {
                    round(fit$final_metrics$accuracy, 3))
     label2 = paste("Validation Acc:", 
                    round(fit$validation_metrics$accuracy, 3))
-    sec_acc = sort(fit$metrics$accuracy)[2]
+    first_acc = sort(fit$metrics$accuracy)[1]
     third_acc = sort(fit$metrics$accuracy)[3]
     
     fit$metrics %>%
     ggplot(aes(x = epoch, y = accuracy)) +
       geom_line(color = "blue") +
-      annotate("text", x = epochs-2, y = sec_acc, 
+      annotate("text", x = input$epochs-2, y = first_acc, 
                label = label1, color = "blue", alpha = 0.5) +
-      annotate("text", x = epochs-2, y = third_acc,
+      annotate("text", x = input$epochs-2, y = third_acc,
                label = label2, color = "red", alpha = 0.5) +
-      hrbrthemes::theme_ipsum() +
-      labs(title = "Accuracy", y = "Training Accuracy")
-    })
-  
-  # Plot predictions ----
-  output$pred_plot = renderPlot({
-    fit = fit_model()
-    spiral_data = sim_spiral()
-    spiral_data = spiral_data[,c("x","y")]
-    data = cbind(spiral_data, pred = fit$predictions)  
-    
-    data %>%
-      ggplot(aes(x = x, y = y, color = as.factor(pred))) +
-      geom_point(alpha = 0.7) +
-      hrbrthemes::scale_color_ft() +
-      labs(title = "Predicted Classes") +
-      hrbrthemes::theme_ipsum() +
+      #hrbrthemes::theme_ipsum() +
+      labs(title = "Accuracy", y = "Training Accuracy") +
+      ggthemes::theme_tufte() + 
       theme(
         legend.position = "none",
-        plot.title = element_text(size = 12),
-        panel.background = element_rect(fill = "gray95", color = "gray90")
+        panel.grid.major = element_line(size = 0.1, color = "gray90")
       )
     
-  })
+    })
+  
   #Calculate performance metrics ----
   metrics = reactive({
     spiral_data  = sim_spiral()
@@ -345,14 +388,14 @@ server = function(input, output) {
                      dimnames = dimnames(perf$conf_mat))
     print(conf_mat)
     
-  }, rownames = TRUE, colnames = TRUE)
+  }, rownames = TRUE, colnames = TRUE, bordered = TRUE)
   
 
   #Print performance metrics ----
   output$performance = renderTable({
     perf = metrics()
     print(perf$metrics)
-  })
+  }, striped = T, bordered = T)
   
 }
 
